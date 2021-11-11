5af67d5c217dfe787ea9d1863bd663c8e4a5c327
민유 <me@minyou.us>
2021-11-11 20:24:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  alert("이 페이지는 데스크탑에 최적화되어 있습니다" \n "되도록 데스크탑으로 봐주세요");
+  alert("이 페이지는 데스크탑에 최적화되어 있습니다. 되도록 데스크탑으로 봐주세요.");
 }
 window.location.replace("https://implude2021.notion.site/6deda76e826f4af0a4c865063ce15a49");
